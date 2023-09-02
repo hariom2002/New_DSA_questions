a2b676d0ed01a8d32193b95a66733dbb218170c7
Hariom kurmi patel <patelhariomk@gmail.com>
2023-09-02 12:12:00 +0530

    message

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -1,33 +1,40 @@
 //{ Driver Code Starts
-//Initial template for C++
+// Initial template for C++
 
-#include<bits/stdc++.h> 
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 // } Driver Code Ends
-//User function template for C++
+// User function template for C++
 
-class Solution{   
+class Solution
+{
 public:
-    bool isSubsetSum(vector<int>arr, int sum){
-        
-        vector<vector<int>> dp(arr.size()+1, vector<int> (sum+1, 0));
-        
-         for(int i=0;i<arr.size()+1;i++){
-            for(int j =0;j<sum+1;j++){
-                if(i == 0)dp[i][j] == arr[i];
-                if(j == 0)dp[i][j] = 1;
+    bool isSubsetSum(vector<int> arr, int sum)
+    {
+
+        vector<vector<int>> dp(arr.size() + 1, vector<int>(sum + 1, 0));
+
+        for (int i = 0; i < arr.size() + 1; i++)
+        {
+            for (int j = 0; j < sum + 1; j++)
+            {
+                if (i == 0)
+                    dp[i][j] == arr[i];
+                if (j == 0)
+                    dp[i][j] = 1;
             }
-         }
-        
-        for(int i=1;i<arr.size()+1;i++){
-            for(int j =1;j<sum+1;j++){
-                
-                if(j>= arr[i-1])
-                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+        }
+
+        for (int i = 1; i < arr.size() + 1; i++)
+        {
+            for (int j = 1; j < sum + 1; j++)
+            {
+
+                if (j >= arr[i - 1])
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
                 else
-                dp[i][j] = dp[i-1][j];
-                
+                    dp[i][j] = dp[i - 1][j];
             }
         }
         return dp[arr.size()][sum];
@@ -35,24 +42,25 @@ public:
 };
 
 //{ Driver Code Starts.
-int main() 
-{ 
+int main()
+{
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int N, sum;
         cin >> N;
         vector<int> arr(N);
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++)
+        {
             cin >> arr[i];
         }
         cin >> sum;
-        
+
         Solution ob;
         cout << ob.isSubsetSum(arr, sum) << endl;
     }
-    return 0; 
-} 
+    return 0;
+}
 
-// } Driver Code Ends
+// } Driver Code Ends here
